e2bbff96923cbcf42915d5f5978c6e4f46c62116
LaiXinZhang <1078120231@qq.com>
2026-05-25 17:31:46 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  root: process.cwd(),
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
 })
